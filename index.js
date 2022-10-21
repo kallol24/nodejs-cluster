60141f3368d4052fa24e6964c98c36c66465aa24
kallol-lohika <kallol-lohika@xendit.co>
2022-10-21 13:11:22 +0530

    updated loadtest scripts in comment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // https://www.youtube.com/watch?v=9RLeLngtQ3A&t=603s&ab_channel=yoursTRULY
 // https://www.youtube.com/watch?v=6xIbVPyh9wo&ab_channel=LearnCode.academy
+// loadtest -n 1000 -c 100 http://localhost:3001/ 
 
 const app = require('express')();
 const cors = require('cors');
